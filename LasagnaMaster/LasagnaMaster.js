f43a3da54c84097e8e474934cf2a155f049ea19c
Abd'al-Malik Bourassi <malik.chan94420@gmail.com>
2024-03-19 18:52:03 +0100

    Lasagna Master

--- LasagnaMaster/LasagnaMaster.js
+++ LasagnaMaster/LasagnaMaster.js
@@ -7,3 +7,44 @@ export const cookingStatus = (timer) => {
     return "You forgot to set the timer.";
   }
 };
+
+export const preparationTime = (layer, timePerLayer) => {
+  if (timePerLayer) {
+    return layer.length * timePerLayer;
+  } else {
+    return layer.length * 2;
+  }
+};
+
+export const quantities = (layers) => {
+  const obj = {
+    noodles: 0,
+    sauce: 0,
+  };
+
+  for (let i = 0; i < layers.length; i++) {
+    if (layers[i] === "noodles") {
+      obj.noodles += 50;
+    }
+    if (layers[i] === "sauce") {
+      obj.sauce += 0.2;
+    }
+  }
+  return obj;
+};
+
+export const addSecretIngredient = (list1, list2) => {
+  const lastItem = list1.slice(-1)[0];
+  list2.push(lastItem);
+};
+
+export const scaleRecipe = (recipe, portions) => {
+  const newRecipe = {};
+
+  for (let key in recipe) {
+    const amount = recipe[key] / 2;
+    newRecipe[key] = amount * portions;
+  }
+
+  return newRecipe;
+};
