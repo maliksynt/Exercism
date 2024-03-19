969e06d2ef6de66f71ce4258beadf0eb2731747e
Abd'al-Malik Bourassi <malik.chan94420@gmail.com>
2024-03-19 22:42:22 +0100

    Custom Signs

---
+++ CustomSigns/CustomSigns.js
@@ -0,0 +1,19 @@
+export function buildSign(occasion, name) {
+  return `Happy ${occasion} ${name}!`;
+}
+
+export function buildBirthdaySign(age) {
+  return `Happy Birthday! What a ${
+    age < 50 ? "young" : "mature"
+  } fellow you are.`;
+}
+
+export function graduationFor(name, year) {
+  return `Congratulations ${name}!\nClass of ${year}`;
+}
+
+export function costOf(sign, currency) {
+  const arraySign = sign.split("");
+  let price = arraySign.length * 2 + 20;
+  return `Your sign costs ${price.toFixed(2)} ${currency}.`;
+}
