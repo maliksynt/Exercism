389daf387053d7f1493835aef341ad0c81d16222
Abd'al-Malik Bourassi <malik.chan94420@gmail.com>
2024-05-03 02:26:26 +0200

    ResistorColors

---
+++ ResistorColor/ResistorColor.js
@@ -0,0 +1,34 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const ColorList = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+const colorCode = (color) => {
+  return ColorList[color];
+};
+
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
